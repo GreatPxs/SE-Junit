526a207d5350a51bfd90accfca42305c492c1c4d
GreatPxs <131158156+GreatPxs@users.noreply.github.com>
2023-12-04 18:39:41 +0800

    Update TritypeConditionTest.java

--- assign2/TritypeConditionTest.java
+++ assign2/TritypeConditionTest.java
@@ -5,6 +5,13 @@ import org.junit.Test ;
 import java.io.*;
 public class TritypeConditionTest {
     @Test
+    /*测试以下decision（包含3个condition）
+    if (Side1 <= 0 || Side2 <= 0 || Side3 <= 0)
+        {
+            triOut = 4;
+            return (triOut);
+        }
+     */
     public void testTriang(){
         Tritype tri = new Tritype();
         assertEquals(4, tri.Triang(-2,3,0));
